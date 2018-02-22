12a7e49dbbda69257455dcec9440d524c1fa2e22
Théo Verhelst <tverhels@ulb.ac.be>
2018-02-22 14:28:23 +0000

    Do whatever my Python linter told me

--- Taxi_data.py
+++ Taxi_data.py
@@ -1,42 +1,40 @@
 import pandas as pd
-import sys
-from sklearn import preprocessing
 import numpy as np
-import os
 import matplotlib
-matplotlib.use('TkAgg') # using matplotlib in the backend
 import matplotlib.pyplot as plt
 import datetime
 
+matplotlib.use('TkAgg')  # using matplotlib in the backend
+
 # open the file
 train = pd.read_csv('/Users/Gui/PycharmProjects/Taxi_Project/train 2.csv')
-rain = pd.read_csv ('/Users/Gui/PycharmProjects/Taxi_Project/rain.csv')
+rain = pd.read_csv('/Users/Gui/PycharmProjects/Taxi_Project/rain.csv')
 
 
-#give the first give elements
+# give the first give elements
 print(train.head())
 print(train.describe())
 
-#Confirm if there are any nan values
+# Confirm if there are any nan values
 print(train.isnull().sum())
-#print(test.isnull().sum())
+# print(test.isnull().sum())
 
-#Explore the data
+# Explore the data
 
-print(train['vendor_id'].unique()) # Unique values 1 and 2
-print(train['passenger_count'].unique()) #Values range from 0-9
+print(train['vendor_id'].unique())  # Unique values 1 and 2
+print(train['passenger_count'].unique())  # Values range from 0-9
 print(train['pickup_longitude'].unique())
 # test['vendor_id'].unique() # Unique values 1 and 2
 # test['passenger_count'].unique() #Values range from 0-9
 
-#Clean up trip duration in train data
-#We see that trip duration is 1 or 0 which is not possible. so we clean up all values that are greater that 2 standard deviations
+# Clean up trip duration in train data
+# We see that trip duration is 1 or 0 which is not possible. so we clean up all values that are greater that 2 standard deviations
 m = np.mean(train['trip_duration'])
 s = np.std(train['trip_duration'])
 train = train[train['trip_duration'] <= m + 2*s]
 train = train[train['trip_duration'] >= m - 2*s]
 
-#separate date time into date and time, while adding to extra collumns
+# separate date time into date and time, while adding to extra collumns
 train['pickup_date'], train['pickup_time'] = train['pickup_datetime'].str.split(' ', 1).str
 train['dropoff_date'], train['dropoff_time'] = train['dropoff_datetime'].str.split(' ', 1).str
 
@@ -47,15 +45,15 @@ train['dropoff_hour'], train['dropoff_minute'],train['dropoff_seconds'] = train[
 
 
 
-#Reformatting the time collumn of the weather(rain) data set in order to have only Hour instead of Hour:min
-#this will help with matching hour of weather data to training data
+# Reformatting the time collumn of the weather(rain) data set in order to have only Hour instead of Hour:min
+# this will help with matching hour of weather data to training data
 rain['time'] =  pd.to_datetime(rain['time'].str.strip(), format='%H:%M')
 rain['time'] = rain['time'].dt.hour
 
 train['pickup_date'] =  pd.to_datetime(train['pickup_date'].str.strip(), format='%Y-%m-%d')
 train['pickup_date'] = train['pickup_date'].dt.strftime('%d%b')
 
-#delete collumns
+# delete collumns
 # del train['pickup_datetime']
 # del train['dropoff_datetime']
 
@@ -67,7 +65,7 @@ print(train)
 # train = train.sort_values(by='pickup_datetime')
 # print(type(train['pickup_datetime']))
 
-#augmenting data - matching rain data to date and time
+# augmenting data - matching rain data to date and time
 
 # train["rain(mm)"] = ""
 # print(df.head())
@@ -88,19 +86,19 @@ print(train)
 #
 # print(train)
 
-#Remove rides to and from far away areas
+# Remove rides to and from far away areas
 xlim = [-74.03, -73.77]
 ylim = [40.63, 40.85]
 
-#xlim = [-74.04, -73.74]
-#ylim = [40.57, 40.88]
+# xlim = [-74.04, -73.74]
+# ylim = [40.57, 40.88]
 
 train = train[(train.pickup_longitude> xlim[0]) & (train.pickup_longitude < xlim[1])]
 train = train[(train.dropoff_longitude> xlim[0]) & (train.dropoff_longitude < xlim[1])]
 train = train[(train.pickup_latitude> ylim[0]) & (train.pickup_latitude < ylim[1])]
 train = train[(train.dropoff_latitude> ylim[0]) & (train.dropoff_latitude < ylim[1])]
 
-#Plot rides
+# Plot rides
 longitude = list(train.pickup_longitude) + list(train.dropoff_longitude)
 latitude = list(train.pickup_latitude) + list(train.dropoff_latitude)
 plt.figure(figsize = (10,10))
@@ -114,7 +112,5 @@ loc_df['latitude'] = latitude
 # way of localising and isolating only desired collumns (by name) and rows (by index)
 df1 = df.loc[1:1048576, "pickup_datetime":"dropoff_datetime"]
 
-#write dataframe into new csv file
+# write dataframe into new csv file
 train.to_csv('train_clean', sep='\t')
-
-
