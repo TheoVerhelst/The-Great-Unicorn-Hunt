dc9eb857fbc223941fb210e953c7951b03f9192d
Théo Verhelst <tverhels@ulb.ac.be>
2018-02-22 20:41:05 +0000

    Rename 'train' to 'dataset', that make more sense now

--- code/dataPrep/Taxi_data.py
+++ code/dataPrep/Taxi_data.py
@@ -6,87 +6,87 @@ import matplotlib.pyplot as plt
 matplotlib.use('TkAgg')  # using matplotlib in the backend
 
 # True to parse the training set, False to parse the test set instead
-prepare_training_set = True
+prepare_training_set = False
 
 # open the file
 if prepare_training_set:
-    train = pd.read_csv('data/train.csv')
+    dataset = pd.read_csv('data/train.csv')
 else:
-    train = pd.read_csv('data/test.csv')
+    dataset = pd.read_csv('data/test.csv')
 
 rain = pd.read_csv('data/rain.csv')
 
 """
 print("**********")
 print("Description of the dataset")
-print(train.describe())
+print(dataset.describe())
 print(rain.describe())
 print("**********")
 
 # Confirm if there are any nan values
-print(train.isnull().sum())
+print(dataset.isnull().sum())
 
 # Explore the data
-print(train['vendor_id'].unique())  # Unique values 1 and 2
-print(train['passenger_count'].unique())  # Values range from 0-9
-print(train['pickup_longitude'].unique())
+print(dataset['vendor_id'].unique())  # Unique values 1 and 2
+print(dataset['passenger_count'].unique())  # Values range from 0-9
+print(dataset['pickup_longitude'].unique())
 """
 
 if prepare_training_set:
     # Clean up trip duration in train data
     # We see some absurd trip duration in the training set. So we clean up all
     # values that are greater that 2 standard deviations
-    m = np.mean(train['trip_duration'])
-    s = np.std(train['trip_duration'])
-    train = train[train['trip_duration'] <= m + 2 * s]
-    train = train[train['trip_duration'] >= m - 2 * s]
+    m = np.mean(dataset['trip_duration'])
+    s = np.std(dataset['trip_duration'])
+    dataset = dataset[dataset['trip_duration'] <= m + 2 * s]
+    dataset = dataset[dataset['trip_duration'] >= m - 2 * s]
 
 # Convert the pickup and dropoff times to datetime objects
-train['pickup_datetime'] = pd.to_datetime(train['pickup_datetime'].str.strip(), format='%Y-%m-%d %H:%M:%S')
+dataset['pickup_datetime'] = pd.to_datetime(dataset['pickup_datetime'].str.strip(), format='%Y-%m-%d %H:%M:%S')
 rain['datetime'] = pd.to_datetime(rain['datetime'].str.strip(), format='%d/%m/%Y %H:%M')
 
 # Here we decide to use the wheather data from PICKUP TIME, since at test time
 # this is the only timestamp available (obviously we don't have dropoff time)
-train['pickup_datetime_hour'] = train['pickup_datetime'].map(lambda timestamp:timestamp.replace(minute=0, second=0))
+dataset['pickup_datetime_hour'] = dataset['pickup_datetime'].map(lambda timestamp:timestamp.replace(minute=0, second=0))
 
 # Augmenting data - matching rain data to date and time
-train = pd.merge(train, rain, left_on='pickup_datetime_hour', right_on='datetime', validate='many_to_one')
+dataset = pd.merge(dataset, rain, left_on='pickup_datetime_hour', right_on='datetime', validate='many_to_one')
 
 # Iterate over all time components, and put each of them in a new column
 # A bit of dirty reflection here, we use the month, day, ... attributes of
 # datetime objects
 for attribute in ("month", "day", "hour", "minute", "second"):
-    train['pickup_' + attribute] = getattr(train['pickup_datetime'].dt, attribute)
+    dataset['pickup_' + attribute] = getattr(dataset['pickup_datetime'].dt, attribute)
+
+dataset['store_and_fwd_flag'] = dataset['store_and_fwd_flag'].map(lambda flag: 1 if flag == 'Y' else 0)
 
 # Erase the datetime objects, we don't, need them anymore
-del train['datetime'], train['pickup_datetime']
+del dataset['datetime'], dataset['pickup_datetime']
 
 if prepare_training_set:
-    del train['dropoff_datetime']
+    del dataset['dropoff_datetime']
 
 # Remove not features columns
-del train['id'], train['pickup_datetime_hour']
+del dataset['id'], dataset['pickup_datetime_hour']
 
 # Remove rides to and from far away areas
 xlim = [-74.03, -73.77]
 ylim = [40.63, 40.85]
 
-train = train[(train.pickup_longitude > xlim[0]) & (train.pickup_longitude < xlim[1])]
-train = train[(train.dropoff_longitude > xlim[0]) & (train.dropoff_longitude < xlim[1])]
-train = train[(train.pickup_latitude > ylim[0]) & (train.pickup_latitude < ylim[1])]
-train = train[(train.dropoff_latitude > ylim[0]) & (train.dropoff_latitude < ylim[1])]
+dataset = dataset[(dataset.pickup_longitude > xlim[0]) & (dataset.pickup_longitude < xlim[1])]
+dataset = dataset[(dataset.dropoff_longitude > xlim[0]) & (dataset.dropoff_longitude < xlim[1])]
+dataset = dataset[(dataset.pickup_latitude > ylim[0]) & (dataset.pickup_latitude < ylim[1])]
+dataset = dataset[(dataset.dropoff_latitude > ylim[0]) & (dataset.dropoff_latitude < ylim[1])]
 
 # Plot rides
-longitude = list(train.pickup_longitude) + list(train.dropoff_longitude)
-latitude = list(train.pickup_latitude) + list(train.dropoff_latitude)
+longitude = list(dataset.pickup_longitude) + list(dataset.dropoff_longitude)
+latitude = list(dataset.pickup_latitude) + list(dataset.dropoff_latitude)
 plt.figure(figsize=(10, 10))
 plt.plot(longitude, latitude, '.', alpha=0.4, markersize=0.05)
 plt.show()
 
-print(train)
-
 # write dataframe into new csv file
 if prepare_training_set:
-    train.to_csv('data/train_features.csv')
+    dataset.to_csv('data/train_features.csv')
 else:
-    train.to_csv('data/test_features.csv')
+    dataset.to_csv('data/test_features.csv')
