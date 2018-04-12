ee841334b69ffc2867ad65d871a9c9cc424ed9df
Théo Verhelst <tverhels@ulb.ac.be>
2018-04-12 12:03:31 +0200

    Add two data analysis scripts (PCA and linear regression)

--- code/models/pca_analysis.py
+++ code/models/pca_analysis.py
@@ -6,15 +6,29 @@ import numpy as np
 
 dataset = pd.read_csv('data/train_merged.csv')
 y = dataset['trip_duration'].values
-del dataset['trip_duration'], dataset["id"], dataset['distance']
-#X = preprocessing.scale(dataset.values)
+del dataset['trip_duration'], dataset["id"]
 X = dataset.values
 
+X = preprocessing.scale(X)
 
 pca = PCA(n_components=2)
 transformed = pca.fit_transform(X)
+
+plt.figure()
+plt.xticks(rotation='vertical')
+plt.subplots_adjust(bottom=0.26)
+# Apply absolute value since we are interested in importance, not sign of features
+plt.bar(list(dataset), abs(pca.components_[0]))
+plt.title("First PCA component values")
+plt.figure()
+plt.xticks(rotation='vertical')
+plt.subplots_adjust(bottom=0.26)
+plt.bar(list(dataset), abs(pca.components_[1]))
+plt.title("Second PCA component values")
+
 heatmap, xedges, yedges = np.histogram2d(transformed[:, 0], transformed[:, 1], weights=y, bins=50)
 extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+plt.figure()
 plt.clf()
 plt.imshow(heatmap, extent=extent, cmap='hot')
 plt.show()
