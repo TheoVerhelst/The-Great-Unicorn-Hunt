6e06af15359c66bddb1fec3acaa65183f5228c54
Chrescht1337 <frantzenchristian@hotmail.com>
2018-04-19 18:23:09 +0100

    adaboost randomforest

--- code/models/adaboost.py
+++ code/models/adaboost.py
@@ -4,7 +4,8 @@ from sklearn import linear_model
 from sklearn.model_selection import validation_curve,cross_val_score,train_test_split
 from sklearn import preprocessing
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import AdaBoostRegressor
+
+from sklearn.ensemble import AdaBoostRegressor,RandomForestRegressor
 import pandas as pd
 from time import time
 #import matplotlib.pyplot as plt
@@ -22,8 +23,9 @@ X = dataset.values
 # Normalize X
 X = preprocessing.scale(X)
 rng = np.random.RandomState(1)
-regr = AdaBoostRegressor(DecisionTreeRegressor(max_depth=4),
-                          n_estimators=200, random_state=rng)
+#regr = AdaBoostRegressor(DecisionTreeRegressor(max_depth=4),
+#                          n_estimators=200, random_state=rng)
+regr = AdaBoostRegressor(RandomForestRegressor(max_depth=4,n_jobs=-1),n_estimators=200, random_state=rng)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
 print("starting fit")
