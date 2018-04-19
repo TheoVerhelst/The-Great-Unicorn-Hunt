a8ce7e450d11f76d05b2dd3102a8000d6dd4e655
Mortimer Sotom <mortzxd@gmail.com>
2018-04-19 20:17:42 +0100

    xgboost working
    code updated but still needs some editing

--- code/models/xgboost.py
+++ code/models/xgboost.py
@@ -1,49 +1,33 @@
 # XGBoost model
 
 # import libraries
-#%matplotlib inline
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from xgboost import XGBClassifier
+import xgboost as xgb
 from sklearn.linear_model import LinearRegression, Ridge,BayesianRidge
 from sklearn.cluster import MiniBatchKMeans
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import confusion_matrix
-import matplotlib
-import numpy as np
-import matplotlib.pyplot as plt
-#plt.rcParams['figure.figsize'] = [16, 10]
 
 # import prepared dataset
 dataset = pd.read_csv('data/train_merged.csv')
 y = dataset['trip_duration'].values
 del dataset['trip_duration'], dataset["id"], dataset['distance']
-#X = preprocessing.scale(dataset.values)
 X = dataset.values
-
-# visualise data + obtain data summary
-#pd.set_option('display.float_format', lambda x: '%.3f' % x)
-#dataset.head()
-#dataset.describe()
-#dataset.info()
 
 # Split dataset into tr and tst sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
-# fit XGBoost to 
-classifier = XGBClassifier()
-classifier.fit(X_train, y_train)        
-# !! this currently spits out the following error: 'WindowsError: [Error -529697949] Windows Error 0xE06D7363'
-# trying to fix this but at least the rest of the code is available
+# fit XGBoost to training set
+dtrain = xgb.DMatrix(X_train, label=y_train)
+dvalid = xgb.DMatrix(X_test, label=y_test)
+watchlist = [(dtrain, 'train'), (dvalid, 'valid')]
 
-# Predicting test set resuts
-y_pred = classifier.predict(X_test)
+xgb_pars = {'min_child_weight': 1, 'eta': 0.5, 'colsample_bytree': 0.9, 
+            'max_depth': 6,
+'subsample': 0.9, 'lambda': 1., 'nthread': -1, 'booster' : 'gbtree', 'silent': 1,
+'eval_metric': 'rmse', 'objective': 'reg:linear'}
+model = xgb.train(xgb_pars, dtrain, 10, watchlist, early_stopping_rounds=2,
+      maximize=False, verbose_eval=1)
+print('Modeling RMSLE %.5f' % model.best_score)
 
-# Build confusion matrix
-cm = confusion_matrix(y_test, y_pred)
-
-# k-fold cross validation
-from sklearn.model_selection import cross_val_score
-accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
-
-accuracies.std()
