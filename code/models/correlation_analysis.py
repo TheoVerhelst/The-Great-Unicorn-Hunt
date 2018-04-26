eb736e74a7db55d646aa3ae7aa6c3fb86477ece9
Théo Verhelst <tverhels@ulb.ac.be>
2018-04-26 16:49:24 +0100

    Make scatter markers smaller in correlation plot

--- code/models/correlation_analysis.py
+++ code/models/correlation_analysis.py
@@ -1,5 +1,3 @@
-from sklearn import preprocessing
-from sklearn.utils import resample
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -19,14 +17,14 @@ fig, ax = plt.subplots()
 sns.heatmap(corr, cmap=sns.diverging_palette(220, 10, as_cmap=True))
 plt.subplots_adjust(bottom=0.26, left=0.35)
 
-graph = sns.pairplot(dataset)
+graph = sns.pairplot(dataset, plot_kws={"s": 5, "linewidth":1, "color":"b", "edgecolor":"b"}, markers=".")
 # Remove axes number labels (we're not really interested in actual numbers on these)
 graph.set(xticklabels=[], yticklabels=[])
 # Set margins
 plt.subplots_adjust(bottom=0.15, left=0.1)
 # Rotate variable names
 for ax in graph.axes.flatten():
-    ax.set_xlabel(ax.get_xlabel(), rotation=30)
+    ax.set_xlabel(ax.get_xlabel(), rotation=50)
     ax.set_ylabel(ax.get_ylabel(), rotation="horizontal")
 
 plt.show()
