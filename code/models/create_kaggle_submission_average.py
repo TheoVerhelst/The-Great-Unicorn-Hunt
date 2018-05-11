3546a529e400f9754f58ce30f6eb2c039b7c7412
Théo Verhelst <tverhels@ulb.ac.be>
2018-05-11 15:21:27 +0100

    Final touch

--- code/models/create_kaggle_submission_average.py
+++ code/models/create_kaggle_submission_average.py
@@ -2,12 +2,18 @@ import pandas as pd
 
 output_filename = "data/submission_average.csv"
 
-rf_pred = pd.read_csv("data/submission_rf.csv")
-xgb_pred = pd.read_csv("data/submission_xgb.csv")
+rf_pred = pd.read_csv("data/submission_rf_gui.csv")
 nn_pred = pd.read_csv("data/submission_nn.csv")
+
+nn_pred.loc[nn_pred.trip_duration < 0, 'trip_duration'] = 0
+
+print("RF")
+print(rf_pred.describe())
+print("NN")
+print(nn_pred.describe())
 
 merged_pred = pd.DataFrame()
 merged_pred["id"] = rf_pred["id"]
-merged_pred["trip_duration"] = (pred_rf["trip_duration"] + pred_xgb["trip_duration"] + pred_nn["trip_duration"]) / 3
+merged_pred["trip_duration"] = (rf_pred["trip_duration"] + nn_pred["trip_duration"]) / 2
 
 merged_pred.to_csv(output_filename, index=False)
